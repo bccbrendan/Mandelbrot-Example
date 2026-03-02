3d265b9cbb54360851a837de6501e2e0b6d7f200
Brendan Long <brendan.long@arm.com>
2026-03-02 14:42:18 -0600

    Add default thread count CLI arg of 1

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
-    const int NUM_THREADS = std::stoi(argv[1]);
+    const int NUM_THREADS = argc == 2 ? std::stoi(argv[1]) : 1;
     std::cout << "Number of Threads = " << NUM_THREADS << std::endl;
 
     Mandelbrot::Mandelbrot myplot(1920, 1080, NUM_THREADS);
-    myplot.draw("/home/ec2-user/Mandelbrot-final/Mandelbrot-Example/images/Green-Parallel-512.bmp", Mandelbrot::Mandelbrot::GREEN);
+    myplot.draw("Green-Parallel-512.bmp", Mandelbrot::Mandelbrot::GREEN);
 
     return 0;
 }
