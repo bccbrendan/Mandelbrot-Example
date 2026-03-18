f3b98781685c74f20a3292dc609119a6060cb5d7
Ubuntu <ubuntu@ip-10-248-213-124.eu-west-1.compute.internal>
2026-03-18 23:44:34 +0000

    fix WiP

--- Makefile
+++ Makefile
@@ -9,9 +9,11 @@ CXX_FLAGS = --std=c++11
 CPP_FLAGS = -I${INCLUDE_DIR}
 
 PARALLEL_VERSION = main.cpp bitmap.cpp mandelbrot.cpp
+SINGLE_THREAD_VERSION = main.cpp bitmap.cpp mandelbrot_single_thread.cpp
 
 
 PARALLEL_OBJS = $(addprefix $(BUILD_DIR)/, $(PARALLEL_VERSION:.cpp=.o))
+SINGLE_THREAD_OBJS = $(addprefix $(BUILD_DIR)/, $(SINGLE_THREAD_VERSION:.cpp=.o))
 
 ifeq (${DEBUG}, 1)
 	CXX_FLAGS += -g -O0
@@ -21,10 +23,13 @@ else
 	BUILD_SUFFIX = 
 endif
 
-all: create parallel
+all: create parallel single_thread
 
 parallel: $(PARALLEL_OBJS)
 	$(CXX) $(CXX_FLAGS) $^ -o $(BUILD_DIR)/mandelbrot_parallel$(BUILD_SUFFIX)
+
+single_thread: $(SINGLE_THREAD_OBJS)
+	$(CXX) $(CXX_FLAGS) $^ -o $(BUILD_DIR)/mandelbrot_single_thread$(BUILD_SUFFIX)
 
 
 # Compile .cpp → build/.o
---
+++ src/mandelbrot copy.cpp
@@ -0,0 +1,156 @@
+#include <complex>
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include "Mandelbrot.h"
+
+using namespace std;
+
+namespace Mandelbrot{
+
+Mandelbrot::Mandelbrot(int width, int height, const int N_THREADS):
+    _bitmap(width, height),
+    _width(width),
+    _height(height), 
+    colourHistogram(new int[MAX_ITERATIONS]{0}),
+    fractalData(new int[_width*_height]),
+    NUM_THREADS(N_THREADS)
+    {
+}
+
+int Mandelbrot::getIterations(double x, double y){
+    /*
+    Will return any number up to MAX_ITERATIONS (1024)
+    */
+    const int THRESHOLD = 2; // 2
+    
+    complex<double> z(0); // 0 + 0j
+    complex<double> c(x,y); // x + yj
+
+    int iterations{0};
+
+    while (iterations < MAX_ITERATIONS){
+        z = (z*z) + c;
+        if (abs(z) > THRESHOLD){
+            break;
+        }
+        iterations++;
+    }
+    return iterations;
+}
+
+void Mandelbrot::draw(string fileName, drawColor colourSelection ){
+
+    int* p = colourHistogram.get();
+    int* pfractalData = fractalData.get();
+
+    std::vector<std::thread> threads;
+    threads.reserve(NUM_THREADS);
+
+    auto work = [&](int thread_id){
+        for (int y = thread_id; y < _height; y+= NUM_THREADS){
+            for (int x = 0; x < _width; x++){
+                double xFractal = (x - _width/2 - 150) * 2.0/_width;
+                double yFractal = (y - _height/2) * 2.0/_width;
+
+                int num_iters = getIterations(xFractal, yFractal);
+                
+                unique_lock<mutex> l(histMutex);
+                // create a pixel map for data
+                pfractalData[y*_width + x] = num_iters;
+
+                // remove max interations from histogram for prettier plot.
+                if (num_iters != MAX_ITERATIONS){
+                    p[num_iters]++;
+                }
+                l.unlock();
+            }
+        }
+    };
+
+    for(int tid = 0; tid < NUM_THREADS; tid++){
+        threads.emplace_back(work, tid);
+    }
+
+    for(auto& i: threads){
+        i.join();
+    }
+
+    int total{0};
+    for (int i =0; i < MAX_ITERATIONS; i++){
+        total += colourHistogram[i];
+    }
+    
+    for (int y = 0; y < _height; y++){
+        for (int x = 0; x < _width; x++){
+
+            uint8_t red = 0;
+            uint8_t green = 0;
+            uint8_t blue = 0;
+
+            int num_iters = pfractalData[y*_width + x];
+            if (num_iters != MAX_ITERATIONS){
+                double hue{0.0};
+                for(int i = 0; i < num_iters; i++){
+                    hue += (double)colourHistogram[i] / total; // divide by total num of pixels
+                }
+
+                // uint8_t colour = (uint8_t) (255 * (double)num_iters / MAX_ITERATIONS);
+
+                //colour = colour*colour*colour; // make small colours bigger
+
+                red = pow(255, hue);
+                green = pow(255, hue);
+                blue = pow(255, hue);
+            }
+            // Adjust hue
+
+            switch(colourSelection){
+                case RED:
+                    _bitmap.setPixel(x, y, red, 0, 0);
+                    break;
+                case GREEN:
+                    _bitmap.setPixel(x, y, 0, green, 0);
+                    break;
+                case BLUE:
+                    _bitmap.setPixel(x, y, 0, 0, blue);
+                    break;
+
+            }
+
+        }
+    }
+    _bitmap.write(fileName);
+    return;
+}
+
+void Mandelbrot::printHistogram(){
+    int* p = colourHistogram.get();
+
+    for(int i = 0; i < MAX_ITERATIONS; i++){
+        std::cout << p[i] << std::endl;
+    }
+
+}
+
+bool Mandelbrot::_validHistogram(){
+    /*
+    Sum counts in histogram bins and check if equal to number of pixels
+    */
+    int* p = colourHistogram.get();
+    int numPixels = _width*_height;
+    int count{0};
+
+    for(int i = 0; i < MAX_ITERATIONS; i++){
+        count += p[i];
+    }
+
+    if (count == numPixels){
+        return true;
+    }
+
+    return false;
+}
+}
+
